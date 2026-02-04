995cab2e76dab281e416c2d7a7251cccb4553201
Alioune Diop <aliounediop723@gmail.com>
2026-02-04 18:53:46 +0000

    Correctif de securite

--- database.py
+++ database.py
@@ -4,9 +4,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+# Charge les variables du fichier .env
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+mysqlconnector://root@localhost/tontine_db")
+# Récupère l'URL. Si elle n'existe pas, renvoie None.
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Sécurité : On vérifie que l'URL est bien chargée
+if not DATABASE_URL:
+    raise ValueError("❌ Erreur : La variable DATABASE_URL est introuvable. Vérifie ton fichier .env !")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
